689beedf7e8ff8fbea659b4056747dce9efeb010
Rade <bajic.rade2@gmail.com>
2021-06-16 10:12:56 +0200

    🦉

--- redis/server.ts
+++ redis/server.ts
@@ -6,11 +6,35 @@ const client = createClient()
 
 const rIncr = promisify(client.incr).bind(client)
 
+const rGet = promisify(client.get).bind(client)
 // OVO JE EXP
 const rSetex = promisify(client.setex).bind(client)
 
 // KORISTIO BIH OVO OVAKO
+// KLJUC JE key, ttl JE TITME TO EXOIRATION, SLOW FUNCTION KOJU CIJI REZULTAT SE CACJHE-UJE 
+function cache(key: string, ttl: number, slowFn: (...props: any) => Promise<any>){
 
+  return async function cachedFn(...props: any[]) {
+    const cachedResponse = await rGet(key)
+
+    if(cachedResponse){
+      return cachedResponse
+    }
+
+    const result = await slowFn(...props);
+
+    await rSetex(key, ttl, result);
+
+     
+  }
+
+}
+
+
+// EXPENSIVE QUERY, CIJI CU REZULTAT CACHE-OVATI
+async function verySlowExpensivePostgresSQLquery(){
+
+}
 
 
 function init(){
